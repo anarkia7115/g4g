6cd2d327243a8381fad87b5e6bbc5a40ce578e54
gaojx <gaojxcs@gmail.com>
2018-12-05 09:44:30 +0800

    add time diff

--- fibonacci/memorization/sol.py
+++ fibonacci/memorization/sol.py
@@ -2,27 +2,38 @@
 # -*- coding: utf-8 -*-
 
 def fib(n, lookup):
-	if lookup[n] != None:
-		return lookup[n]
-	else:
-		if n == 0:
-			return 0
-		if n == 1:
-			return 1
-		else:
-			lookup[n] = fib(n-1, lookup) + fib(n-2, lookup)
-			return lookup[n]
+    if lookup[n] != None:
+        return lookup[n]
+    else:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            lookup[n] = fib(n-1, lookup) + fib(n-2, lookup)
+            return lookup[n]
 
 # Driver program to test the above function 
-def main(): 
-	n = 34
-	# Declaration of lookup table 
-	# Handles till n = 100 
-	lookup = [None]*(101) 
-	print ("Fibonacci Number is ", fib(n, lookup) )
+def happy():
+    n = 888
+    # Declaration of lookup table 
+    # Handles till n = 100 
+    lookup = [None]*(1000)
+    print("Fibonacci Number is ", fib(n, lookup) )
 
-if __name__=="__main__": 
-	main() 
+
+if __name__=="__main__":
+    import time
+
+    start_time = time.time()
+    # time.sleep(1)
+    happy()
+    end_time = time.time()
+
+    time_diff = end_time - start_time
+    print("--- {} seconds ---".format(time_diff))
+
+    # main()
 
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
 # modified by gaojx
--- fibonacci/tabulation/sol.py
+++ fibonacci/tabulation/sol.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def main(): 
-    n = 34
+    n = 888
     fib = []
     fib.append(0)
     fib.append(1)
@@ -13,7 +13,16 @@ def main():
     # Handles till n = 100 
     print ("Fibonacci Number is ", fib[n] )
 
-if __name__=="__main__": 
-    main() 
+if __name__=="__main__":
+    import time
+
+    start_time = time.time()
+    # time.sleep(1)
+    main()
+    end_time = time.time()
+
+    time_diff = end_time - start_time
+    print("--- {} seconds ---".format(time_diff))
+
 
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
