324e8cbc5694fd8ef5728a99011b3c3b9dae6d54
gaojx <gaojxcs@gmail.com>
2018-12-26 09:50:24 +0800

    accepted!

--- longest_common_subsequence/code.py
+++ longest_common_subsequence/code.py
@@ -1,41 +1,73 @@
 def lcs(s1, s2):
     """
+    a) use a row to record longest 
+        match sequence (match_seq, idx:i)
+    b) if e_i == e_j, new_m += m[j-1, i-1]
+       elif new_m = max(m[j-1, i], m[i, j-1])
+    c) return m[max_i]
     the longest common sequence of (s1, s2)
-    the longest common sequence of (s1[1:], s2)
     :param s1:
     :param s2:
     :return:
     """
-    s2_start = 0
-    common_size = 0
-    common_str = ""
-    for i in range(len(s1)):
-        ch1 = s1[i]
-        for j in range(s2_start, len(s2)):
-            ch2 = s2[j]
-            # check if ch1, ch2 is equal
-            # if equal continue to next char of s1
-            # begin from next char in s2
-            # else do nothing
-            if ch1 == ch2:
-                s2_start = j + 1
-                common_size += 1
-                common_str += ch1
-                break
+    # a) use a row to record longest 
+    #     match sequence (match_seq, idx:i)
+    m = [0] * len(s1)
+    om = m[:]  # old m
+    # s1: i, m: i
+    # s2: j
+    # print(s1, s2)
+    # print("      ", "  ".join(list(s1)))
+    for j in range(len(s2)):
+        e_j = s2[j]
+        for i in range(len(s1)):
+            e_i = s1[i]
+            # b) if e_i == e_j, m[i] += 1
+            # b) if e_i == e_j, new_m += m[j-1, i-1]
+            if e_i == e_j:
+                """
+                if e_i is the first element, m[i] should <= 1
+                if e_i-1 exists, m[i] +=1 only if m[i-1] == m[i]
+                """
+                if i == 0:
+                    m[i] = 1
+                else:
+                    m[i] = om[i-1] + 1
+            # elif new_m = max(m[j-1, i], m[j, i-1])
+            else:
+                if i == 0:
+                    m[i] = om[i]
+                else:
+                    m[i] = max(om[i], m[i-1])
+        # goto next row
+        om = m[:]
 
-    return common_size, common_str
+        # print("m[{}]: {}".format(e_j, m))
+    return m[len(s1) - 1]
 
+
+def main2():
+    import fileinput
+    f = fileinput.input()
+    test_case_num = int(f.readline().strip())
+    for _ in range(test_case_num):
+        (_, _) = [int(x) for x in f.readline().strip().split(" ")]
+        s1 = f.readline().strip()
+        s2 = f.readline().strip()
+        # print(s1, s2, lcs(s1, s2))
+        print(lcs(s1, s2))
 
 def main(input_file):
     with open(input_file) as f:
         test_case_num = int(f.readline().strip())
-        for i in range(test_case_num):
-            (s1_l, s2_l) = [int(x) for x in f.readline().strip().split(" ")]
+        for _ in range(test_case_num):
+            (_, _) = [int(x) for x in f.readline().strip().split(" ")]
             s1 = f.readline().strip()
             s2 = f.readline().strip()
-            print(s1, s2, lcs(s1, s2))
+            # print(s1, s2, lcs(s1, s2))
+            print(lcs(s1, s2))
 
 
 if __name__ == "__main__":
-    in_file = "./input.txt"
+    in_file = ".\\longest_common_subsequence\\input.txt"
     main(in_file)
