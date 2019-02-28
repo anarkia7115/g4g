75124a6081c31d928af2db51bd1309cc7292a0a0
Gao JiaXiang <gaojxcs@gmail.com>
2019-02-28 10:37:47 +0800

    replace overlapping keys, add coin order

--- coin_change/sol.py
+++ coin_change/sol.py
@@ -1,32 +1,37 @@
+MAX_COIN_SIZE = 399
+
+
 class CoinChange(object):
     def __init__(self):
-        # TODO: has duplication, when change orders are different
-        self.change_methods_of = dict()
+        # self.change_methods_of = dict()
         pass
 
-    def change_methods(self, coins, big_money):
+    def change_methods(self, coins, big_money, last_coin_size):
         # big_money = change + changed_money
         # change_methods(big_money) = 1 + change_methods(changed_money)
 
         # get method candidates
-        if big_money in self.change_methods_of:
-            return self.change_methods_of[big_money]
-        small_money_candidates = []
-        for change in coins:
-            changed = big_money - change
-            if changed >= 0:  # legal change method
-                small_money_candidates.append(changed)
+        # if big_money in self.change_methods_of:
+        #     return self.change_methods_of[big_money]
+        change_coin_candidates = []
+        for coin in coins:
+            if coin > last_coin_size:
+                continue
+            small_money = big_money - coin
+            if small_money >= 0:  # legal change method
+                change_coin_candidates.append(coin)
 
         if big_money == 0:
             return 1
-        elif len(small_money_candidates) == 0:
+        elif len(change_coin_candidates) == 0:
             return 0
 
         else:
+            # sum
             num_change_methods = sum(
-                [self.change_methods(coins, small_money) for small_money in small_money_candidates]
+                [self.change_methods(coins, big_money - change_coin, change_coin) for change_coin in change_coin_candidates]
             )
-            self.change_methods_of[big_money] = num_change_methods
+            # self.change_methods_of[big_money] = num_change_methods
             return num_change_methods
         pass
 
@@ -40,10 +45,7 @@ def main(file_handle):
         coins = [int(coin) for coin in file_handle.readline().strip().split()]
         big_money = int(file_handle.readline().strip())
 
-        # print(s1, s2, lcs(s1, s2))
-        # pim.print(mat)
-        print(sol_instance.change_methods(coins, big_money))
-        # pim.print_intermediate()
+        print(sol_instance.change_methods(coins, big_money, MAX_COIN_SIZE))
 
 
 if __name__ == "__main__":
